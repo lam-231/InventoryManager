913318065130c0d9ddded8a1b42800c5c36e6a1b
lam-231 <vt231_lam@student.ztu.edu.ua>
2025-06-07 23:25:13 +0300

    Update InventoryStatisticsService.cs
    changed InventoryStatisticsService to match the interface

--- InventoryManagement/Services/InventoryStatisticsService.cs
+++ InventoryManagement/Services/InventoryStatisticsService.cs
@@ -4,15 +4,19 @@ using System.Linq;
 
 namespace InventoryManagement.Services
 {
-  public class InventoryStatisticsService
+  public class InventoryStatisticsService : IInventoryStatisticsService
   {
     public int GetTotalQuantity(IEnumerable<Product> products)
     {
+      if (products == null)
+          throw new ArgumentNullException(nameof(products));
       return products.Sum(p => p.Quantity);
     }
 
     public decimal GetTotalValue(IEnumerable<Product> products)
     {
+      if (products == null)
+          throw new ArgumentNullException(nameof(products));
       return products.Sum(p => p.Price * p.Quantity);
     }
   }
